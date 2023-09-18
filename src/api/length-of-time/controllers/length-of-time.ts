eb97ae4adfd442053ee3c6b5f30ee933102f8e38
Cmdmdz <wirawat.tum@northbkk.ac.th>
2023-09-19 03:55:18 +0700

    add length of time

---
+++ src/api/length-of-time/controllers/length-of-time.ts
@@ -0,0 +1,8 @@
+/**
+ * A set of functions called "actions" for `length-of-time`
+ */
+import {factories} from "@strapi/strapi";
+
+export default factories.createCoreService(
+  "api::length-of-time.period-option"
+);
---
+++ src/api/length-of-time/services/length-of-time.ts
@@ -0,0 +1,9 @@
+/**
+ * length-of-time service
+ */
+
+import {factories} from "@strapi/strapi";
+
+export default factories.createCoreService(
+  "api::length-of-time.period-option"
+);
